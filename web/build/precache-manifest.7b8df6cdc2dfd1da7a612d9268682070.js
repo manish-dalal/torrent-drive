a015edac15f4ff27dbd64e2dfbc5653acbf39f68
Manish Dalal <manish.d@bettertechsolution.com>
2020-11-07 20:36:32 +0530

    build added

---
+++ web/build/precache-manifest.7b8df6cdc2dfd1da7a612d9268682070.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f07497a57d86aa4e3a7de408f32bd4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19f092382ea34e8953e2",
+    "url": "/static/css/main.98542162.chunk.css"
+  },
+  {
+    "revision": "146603559cd2e918e2fb",
+    "url": "/static/js/0.c64e2a40.chunk.js"
+  },
+  {
+    "revision": "4ffbe2b8a3508ab5f573",
+    "url": "/static/js/3.0a6f9a57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.0a6f9a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0cc68fa7134ae78b8a",
+    "url": "/static/js/4.cf6ae25a.chunk.js"
+  },
+  {
+    "revision": "f3ace966417c8161da13",
+    "url": "/static/js/5.10800f32.chunk.js"
+  },
+  {
+    "revision": "c07198a1e7a9a997bd3e",
+    "url": "/static/js/6.0dbab8fb.chunk.js"
+  },
+  {
+    "revision": "19f092382ea34e8953e2",
+    "url": "/static/js/main.ef397f41.chunk.js"
+  },
+  {
+    "revision": "24c6f569b17c371233e9",
+    "url": "/static/js/runtime-main.8e854dce.js"
+  }
+]);
